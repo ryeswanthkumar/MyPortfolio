c4ef938283a18fae5a04ec494d269df3439ac20c
ryeswanthkumar <yeswanthkumar348@gmail.com>
2026-02-08 16:35:43 +0530

    Prepare project for Render deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8r*9#seipwt&@+$+#ke0-%c)x3yhqbj+vc-4tgkx%r(nnovv)4'
+#SECRET_KEY = 'django-insecure-8r*9#seipwt&@+$+#ke0-%c)x3yhqbj+vc-4tgkx%r(nnovv)4'
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
