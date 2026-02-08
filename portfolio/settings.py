62f18b9e8ca036f81d3d5444d02f22ba0e1aa0e7
ryeswanthkumar <yeswanthkumar348@gmail.com>
2026-02-08 16:53:16 +0530

    Fix static files config for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'main' / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
